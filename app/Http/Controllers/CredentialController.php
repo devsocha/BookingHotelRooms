d7b33e9db52b8a14c084ab5d031fd3692bc9b973
devsocha <konrad.socha5@gmail.com>
2022-12-19 20:08:34 +0100

    Dodanie veryfikacji mailowej poprzez token i jego obsluga z podmiana na aktywny status konta oraz usunięcie tokenu

--- app/Http/Controllers/CredentialController.php
+++ app/Http/Controllers/CredentialController.php
@@ -24,12 +24,21 @@ class CredentialController extends Controller
          ]);
          $verificationLink = url('registration/verify/'.$token);
          $subject = 'Potwierdzenie rejestracji w portalu';
-         $body = "Cześć ".$request->name.',<br>'.'Aby potwierdzić logowanie kliknij w link poniżej: <br>'.'<a href='.$verificationLink.'>'.$verificationLink.'</a>';
+         $body = "Cześć ".$request->name.',<br>'.'Aby potwierdzić logowanie kliknij w link poniżej: <br>'.'<a href='.$verificationLink.'>'.'Link do potwierdzenia'.'</a><br><br> Pozdrawiam, <br> DevSocha';
             Mail::to($request->email)->send(new WebsiteMailer($subject,$body));
         echo 'email send';
     }
     public function registrationVerify($token){
-
+        $user = User::where('token',$token)->exists();
+        if($user){
+        User::where('token',$token)->update([
+            'status' => 'aktywny',
+            'token'=>'',
+        ]);
+        echo 'allGood';
+        }else{
+            return redirect()->route('login');
+        }
     }
     public function forgotPassword(){
 
