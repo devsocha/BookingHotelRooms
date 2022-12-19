6919ad422b265ea68e05bdb0cd2a07ccd968b7db
devsocha <konrad.socha5@gmail.com>
2022-12-19 18:36:21 +0100

    Dodanie migracji usera oraz stworzenie routa głównego wraz z controllerami Webside oaz Crdential

---
+++ app/Http/Controllers/CredentialController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CredentialController extends Controller
+{
+    public function login(){
+
+    }
+    public function register(){
+
+    }
+    public function saveRegister(){
+
+    }
+    public function forgotPassword(){
+
+    }
+    public function verifyAccount(){
+
+    }
+}
